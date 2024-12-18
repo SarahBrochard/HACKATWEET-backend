f63e63b9f2e53a38f8c41f4d2f6d87af91cd472d
sarah Dev <s64brochard@gmail.com>
2024-12-18 16:45:42 +0100

    exo

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,5 +1,4 @@
 
-
 var express = require('express');
 var router = express.Router();
 const Tweet = require('../models/tweets');
@@ -22,6 +21,41 @@ router.post('/postTweet', (req, res) => {
 
 
 })
+// route testée et ok le 18/12/24
+
+// get Last Tweets
+router.get('/lastTweets', (req, res) => {
+
+    Tweet.find().then(data => {
+        console.log('la liste des tweets est la suivante', data);
+    });
+})
+
+// delete tweets 
+
+
+router.delete('/deleteTweet', (req, res) => {
+    console.log('delete wanting recieved ')
+    const tweetId = req.body._id;
+
+    if (!tweetId) {
+        return res.status(400).json({ result: false, message: 'Tweet ID is required' });
+    }
+
+    Tweet.deleteOne({ _id: tweetId })
+        .then(result => {
+            if (result.deletedCount === 0) {
+                return res.status(404).json({ result: false, message: 'Tweet not found' });
+            }
+
+            res.json({ result: true, message: `Tweet with ID ${tweetId} deleted` });
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ result: false, error: "erreur dans la demande de suppression" });
+        });
+});
+// route ok 
 
 
 module.exports = router;
